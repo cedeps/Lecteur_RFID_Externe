9ec45438c55dd29fc4c01abb48c3abf61273873d
cedeps <62479374+cedeps@users.noreply.github.com>
2024-10-26 18:42:15 +0200

    Lecteur_RFID_Bluetooth.ino

--- Lecteur_RFID_Bluetooth.ino
+++ Lecteur_RFID_Bluetooth.ino
@@ -8,43 +8,29 @@
 #define BLUE_LED 13
 #define GREEN_LED 14
 
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance de la classe
-MFRC522::MIFARE_Key key; 
+MFRC522 rfid(SS_PIN, RST_PIN); // Utilisation de MFRC522
 BluetoothSerial SerialBT;
-bool bluetoothConnected = true; // Variable pour suivre l'état de la connexion Bluetooth
-int ledState = HIGH; // État de la LED Bleue
-
-void tone(byte pin, int freq) {
-  ledcSetup(0, 2000, 8); // Configuration du bip
-  ledcAttachPin(BUZZER_PIN, 0); // Attacher le canal 0 à la broche BUZZER_PIN
-  ledcWriteTone(0, freq); // Jouer bip
-}
-
-void noTone() {
-  ledcWrite(0, 0); // Arrêter la tonalité
-}
+bool bluetoothConnected = false;
+int ledState = LOW;
 
 void setup() { 
   Serial.begin(115200);
   SerialBT.begin("Box_001"); // Nom du Bluetooth
   SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init RC522
-  pinMode(BLUE_LED, OUTPUT); // Led Bleue Bluetooth
-  pinMode(GREEN_LED, OUTPUT); // Led Verte
+  rfid.PCD_Init(); // Init MFRC522
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
 
-  //**************Fin Phase d'initialisation
-  tone(25, 3000);
-  digitalWrite(GREEN_LED, HIGH); // Led verte allumée puis éteinte
+  tone(BUZZER_PIN, 3000); // Son initial
+  digitalWrite(GREEN_LED, HIGH);
   delay(300);
   digitalWrite(GREEN_LED, LOW);
-  noTone();
+  noTone(BUZZER_PIN);
 }
 
 void loop() {
-  handleBluetooth();  // Fonction pour la gestion de la communication Bluetooth
-  
+  handleBluetooth();  // Gestion de la communication Bluetooth
   readRFID(); // Lire la carte RFID si présente
- 
 }
 
 // Convertir l'UUID en Hex. 
@@ -58,38 +44,28 @@ void printHex(byte *buffer, byte bufferSize) {
 }
 
 void handleBluetooth() {
-  bluetoothConnected = SerialBT.connected(); // Vérifie l'état de la connexion
-
+  bluetoothConnected = SerialBT.hasClient(); // Vérifie connexion BLE
+  
   if (bluetoothConnected) {
-    digitalWrite(BLUE_LED, HIGH); // Éteint la LED interne si le Bluetooth est connecté
+    digitalWrite(BLUE_LED, HIGH); // Allume LED si connecté
   } else {
-    // Fait clignoter la LED si le Bluetooth n'est pas connecté
-    if (millis() % 1000 < 500) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
+    ledState = (millis() % 1000 < 500) ? HIGH : LOW;
     digitalWrite(BLUE_LED, ledState);
   }
-
   delay(20);
 }
-void readRFID() {
-  if (rfid.PICC_IsNewCardPresent()) {
-    // Vérification si le NUID a été lu 
-    if (rfid.PICC_ReadCardSerial()) {
-      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-      printHex(rfid.uid.uidByte, rfid.uid.size);
-      SerialBT.println(); // Envoi Bluetooth
-      Serial.println(); // Saut à la ligne; configurer App Inventor en delimiter 10
-      tone(BUZZER_PIN, 3000); // Son
-      digitalWrite(GREEN_LED, HIGH); // Led allumée
-      delay(300); // Durée allumage
-      digitalWrite(GREEN_LED, LOW); // Éteindre Led
-      delay(1);
-      noTone(); // Couper son 
 
-      rfid.PICC_HaltA(); // Arrêt lecture TAG
-    }
+void readRFID() {
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    printHex(rfid.uid.uidByte, rfid.uid.size);
+    SerialBT.println(); // Envoi Bluetooth
+    Serial.println(); // Saut à la ligne
+    tone(BUZZER_PIN, 3000); // Son
+    digitalWrite(GREEN_LED, HIGH); // LED allumée
+    delay(300); // Durée allumage
+    digitalWrite(GREEN_LED, LOW); // Éteindre LED
+    noTone(BUZZER_PIN); // Couper son 
+
+    rfid.PICC_HaltA(); // Arrêt lecture TAG
   }
 }
